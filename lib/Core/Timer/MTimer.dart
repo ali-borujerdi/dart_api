65706d744cc7a435731557cbfba8f387e4546448
Ali <ali.rezaborujerdi@gmail.com>
2024-07-28 19:27:02 +0330

    ALI

---
+++ lib/Core/Timer/MTimer.dart
@@ -0,0 +1,30 @@
+class MTimer {
+  int startingFromTheMinute;
+  int intervalBetweenMinutes;
+  int nextDesiredTime = 0;
+  Function action;
+
+  MTimer(this.startingFromTheMinute, this.intervalBetweenMinutes, this.action,
+      int currentMinure) {
+    for (var i = startingFromTheMinute;
+        i < currentMinure;
+        i += intervalBetweenMinutes) {
+      nextDesiredTime = i + intervalBetweenMinutes;
+    }
+  }
+
+  desiredTimeChecking(int currentMinute) {
+    if (currentMinute >= nextDesiredTime) {
+      if (currentMinute - nextDesiredTime <= intervalBetweenMinutes) {
+        var tmpNextTime = currentMinute + intervalBetweenMinutes;
+        if (tmpNextTime > 59) {
+          nextDesiredTime = (60 - tmpNextTime).abs();
+        } else {
+          nextDesiredTime = tmpNextTime;
+        }
+        action.call();
+        // execute();
+      }
+    }
+  }
+}
